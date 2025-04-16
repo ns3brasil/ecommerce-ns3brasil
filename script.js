4114d0e48afdeb993cadfb6f70ccc9cac3f889b6
Vinicius Oruam <ns3.marketplace@gmail.com>
2025-04-16 15:18:41 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,7 @@
+// Script futuro para carrinho, localStorage, integração com backend etc
+document.querySelectorAll('.btn-comprar').forEach(btn => {
+    btn.addEventListener('click', () => {
+      alert('Produto adicionado ao carrinho! (função futura)');
+    });
+  });
+  
